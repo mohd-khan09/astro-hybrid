d4b5f9691df41bc8c8ba16b9654d0387f7552956
Mohammed khan <126798079+mohd-khan09@users.noreply.github.com>
2025-01-13 12:22:53 +0530

    removed ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: awsAmplify(),
-  output: "hybrid",
+  //   adapter: awsAmplify(),
+  //   output: "hybrid",
 });
