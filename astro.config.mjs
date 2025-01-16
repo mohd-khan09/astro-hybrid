2af54d71361c5416d7940161d2ed9ccfa36a833c
Mohammed khan <126798079+mohd-khan09@users.noreply.github.com>
2025-01-16 11:22:04 +0530

    added ststic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,23 @@
+import awsAmplify from "astro-aws-amplify";
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+
+export default defineConfig({
+  integrations: [react()],
+  adapter: awsAmplify(),
+  output: "static",
+});
 // import awsAmplify from "astro-aws-amplify";
 // import { defineConfig } from "astro/config";
+// import netlify from "@astrojs/netlify";
 // import react from "@astrojs/react";
 
 // // https://astro.build/config
 // export default defineConfig({
+//   //   adapter: awsAmplify(),
 //   integrations: [react()],
-//   adapter: awsAmplify(),
-//   //   output: "hybrid",
+//   output: "static",
+//   adapter: netlify({
+//     edgeMiddleware: true,
+//   }),
 // });
-// import awsAmplify from "astro-aws-amplify";
-import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
-import react from "@astrojs/react";
-
-// https://astro.build/config
-export default defineConfig({
-  //   adapter: awsAmplify(),
-  integrations: [react()],
-  output: "static",
-  adapter: netlify({
-    edgeMiddleware: true,
-  }),
-});
