5d153cc52cd7658b081195c51d3770c6b151e353
Mohammed khan <126798079+mohd-khan09@users.noreply.github.com>
2025-01-20 14:56:54 +0530

    vercel connection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,9 @@
-import awsAmplify from "astro-aws-amplify";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import awsAmplify from "astro-aws-amplify";
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   integrations: [react()],
-  adapter: awsAmplify(),
+  adapter: vercel(),
   output: "hybrid",
 });
-
-// import { defineConfig } from "astro/config";
-// import netlify from "@astrojs/netlify";
-// import react from "@astrojs/react";
-
-// // https://astro.build/config
-// export default defineConfig({
-//   //   adapter: awsAmplify(),
-//   integrations: [react()],
-//   output: "static",
-//   adapter: netlify({
-//     edgeMiddleware: true,
-//   }),
-// });
