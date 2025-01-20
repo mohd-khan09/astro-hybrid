29fa27374f379e538c5178aa7552d5b655e7dee0
Mohammed khan <126798079+mohd-khan09@users.noreply.github.com>
2025-01-20 10:20:29 +0530

    removed pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import awsAmplify from "astro-aws-amplify";
 export default defineConfig({
   integrations: [react()],
   adapter: awsAmplify(),
-  output: "static",
+  output: "hybrid",
 });
 
 // import { defineConfig } from "astro/config";
