02da8ec5b9bb163909592319fcb2740c05c7796a
saurabhsutar192 <saurabhsutar192@gmail.com>
2025-01-23 16:48:18 +0530

    chore: pnpm lock changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import awsAmplify from "astro-aws-amplify";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import awsAmplify from "astro-aws-amplify";
+
 export default defineConfig({
   integrations: [react()],
   adapter: awsAmplify(),
