2e5e983993015b5acd41aa521d6fe3f7696c553b
saurabhsutar192 <saurabhsutar192@gmail.com>
2025-01-23 18:07:45 +0530

    chore: changes the config to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import awsAmplify from "astro-aws-amplify";
 export default defineConfig({
   integrations: [react()],
   adapter: awsAmplify(),
-  output: "server",
+  output: "hybrid",
 });
 
 // import { defineConfig } from "astro/config";
